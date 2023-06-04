a99e9cacdddd52b083aac2d400b8cfab6d4b8c77
vadim <vadimpoldolin19@gmail.com>
2023-06-04 20:11:03 +0200

    final commit

--- src/main/java/com/example/hwstreamapioptional/streamapioptional/Controller/EmployeeController.java
+++ src/main/java/com/example/hwstreamapioptional/streamapioptional/Controller/EmployeeController.java
@@ -10,31 +10,34 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("employee/")
+@RequestMapping("/employee")
 public class EmployeeController {
     private final EmpService empService;
 
     public EmployeeController(EmpService empService) {
         this.empService = empService;
     }
+
     @GetMapping("/employee")
-    public String hello(){
+    public String hello() {
         return "Привет";
     }
 
     @GetMapping("/add")
-    public Employee addEmployee(@RequestParam String name, @RequestParam String surname) {
-        return empService.add(name, surname);
+    public Employee addEmployee(@RequestParam String name, @RequestParam String surname, @RequestParam int department, @RequestParam double salary) {
+        return empService.add(name, surname, department, salary);
     }
 
     @GetMapping("/remove")
-    public Employee removeEmployee(@RequestParam String name, @RequestParam String surname) {
-        return empService.remove(name, surname);
+    public Employee removeEmployee(@RequestParam String name, @RequestParam String surname,
+                                   @RequestParam double salary, @RequestParam int department) {
+        return empService.remove(name, surname, department, salary);
     }
 
     @GetMapping("/find")
-    public Employee findEmployee(@RequestParam String name, @RequestParam String surname) {
-        return empService.find(name, surname);
+    public Employee findEmployee(@RequestParam String name, @RequestParam String surname,
+                                 @RequestParam double salary, @RequestParam int department) {
+        return empService.find(name, surname, department, salary);
     }
 
     @GetMapping("/findAll")
