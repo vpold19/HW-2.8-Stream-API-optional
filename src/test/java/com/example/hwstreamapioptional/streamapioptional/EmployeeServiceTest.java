86f28e613b719f0f43a2ee71242ee19e2419c86e
vadim <vadimpoldolin19@gmail.com>
2023-07-20 19:43:55 +0200

    Попытался несколькими способами

--- src/test/java/com/example/hwstreamapioptional/streamapioptional/EmployeeServiceTest.java
+++ src/test/java/com/example/hwstreamapioptional/streamapioptional/EmployeeServiceTest.java
@@ -24,13 +24,13 @@ public class EmployeeServiceTest {
             char additionalChar = (char) ('a' + i);
 empServiceImp.add(new Employee("Vadim" + additionalChar, "Poldolin" + additionalChar, 12653623, 1));
         }
-//        Employee employee1 = new Employee("Vadim", "Poldolin", 12653623, 1);
-//        Employee employee2 = new Employee("oleg", "olegov", 20000, 1);
-//        Employee employee3 = new Employee("sergey", "sergeev", 30000, 2);
-//        Employee employee4 = new Employee("Petr", "Petrov", 5000, 2);
-//        Employee employee5 = new Employee("Semen", "Semenov", 10000, 3);
-//        Employee employee6 = new Employee("Gena", "Genov", 1000, 3);
-//        empServiceImp.add(employee1);
+       Employee employee1 = new Employee("Vadim", "Poldolin", 12653623, 1);
+       Employee employee2 = new Employee("oleg", "olegov", 20000, 1);
+      Employee employee3 = new Employee("sergey", "sergeev", 30000, 2);
+       Employee employee4 = new Employee("Petr", "Petrov", 5000, 2);
+        Employee employee5 = new Employee("Semen", "Semenov", 10000, 3);
+        Employee employee6 = new Employee("Gena", "Genov", 1000, 3);
+      empServiceImp.add(employee1);
 
         Assertions.assertThrows(EmployeeStorageIsFullException.class, () -> empServiceImp.add("Iosif", "Stalin",
                 2, 45678));
